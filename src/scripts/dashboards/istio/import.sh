730398df3ba12d0536e649971c191ea327e99cdf
Lyon Till <158992+ljtill@users.noreply.github.com>
2023-10-12 22:09:05 +0100

    feat: add flux dashboards (#30)

--- src/scripts/dashboards/istio/import.sh
+++ src/scripts/dashboards/istio/import.sh
@@ -1,18 +1,34 @@
+#!/bin/bash
+
 # Address of Azure Grafana
-GRAFANA_HOST=""
+GRAFANA_ENDPOINT=""
+
 # Token credentials
 GRAFANA_TOKEN=""
+
 # Name of the Prometheus data source
 GRAFANA_DATASOURCE=""
+
 # Version of Istio to deploy
-VERSION=1.17.5
-# Create Istio folder
+VERSION="1.17.5"
+
+# Get all folders
 FOLDER="$(curl -s \
     -H "Accept: application/json" \
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer $GRAFANA_TOKEN" \
-    -d "{\"title\": \"Istio\"}" \
-    $GRAFANA_HOST/api/folders | jq -r ".uid")"
+    $GRAFANA_ENDPOINT/api/folders | jq -r '.[] | select(.title == "Istio").uid')"
+
+if [[ -z "$FOLDER" ]]; then
+    # Create Istio folder
+    FOLDER="$(curl -s \
+        -H "Accept: application/json" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer $GRAFANA_TOKEN" \
+        -d "{\"title\": \"Istio\"}" \
+        $GRAFANA_ENDPOINT/api/folders | jq -r '.uid')"
+fi
+
 # Import all Istio dashboards
 for DASHBOARD in 7639 7636 7630 11829 7645 13277; do
     REVISION="$(curl -s https://grafana.com/api/dashboards/${DASHBOARD}/revisions -s | jq ".items[] | select(.description | contains(\"${VERSION}\")) | .revision")"
@@ -25,6 +41,6 @@ for DASHBOARD in 7639 7636 7630 11829 7645 13277; do
         -d "{\"dashboard\":$(cat /tmp/dashboard.json),\"folderUid\":\"$FOLDER\",\"overwrite\":true, \
             \"inputs\":[{\"name\":\"DS_PROMETHEUS\",\"type\":\"datasource\", \
             \"pluginId\":\"prometheus\",\"value\":\"$GRAFANA_DATASOURCE\"}]}" \
-        $GRAFANA_HOST/api/dashboards/import
+        $GRAFANA_ENDPOINT/api/dashboards/import
     echo -e "\nDone\n"
 done
